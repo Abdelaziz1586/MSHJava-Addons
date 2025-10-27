2347b67fa2b66b30b18531a9fa6cf9bc52ebb0cb
Abdelaziz Mohamed <81268592+Abdelaziz1586@users.noreply.github.com>
2025-10-27 04:04:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar RemoteMSH.jar "\${REMOTE_MSH_SERVER_URL}"
+exec java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar RemoteMSH.jar ${REMOTE_MSH_SERVER_URL}
